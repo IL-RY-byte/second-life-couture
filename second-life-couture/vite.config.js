d3eac8cffadb180778ef31f883254d59df5e9c0d
IL-RY-byte <ilyaryabko22@gmail.com>
2026-05-20 20:15:28 -0400

    fix: vite config

--- second-life-couture/vite.config.js
+++ second-life-couture/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/second-life-couture/',
   plugins: [react()],
 })
-export default {
-  base: '/second-life-couture/',
-}
